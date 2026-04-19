596bd1e1841e7ca8785779e2921d40b38b1e5def
Xieryy <cuberjohnny9@gmail.com>
2026-04-20 02:47:55 +0700

    create core Fix Circular Dependency Refactor

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,6 +10,7 @@ import { Receipt } from './database/entities/receipts.entity';
 import { ReceiptsModule } from './receipts/receipts.module';
 import { NotificationsModule } from './notifications/notifications.module';
 import { OrdersModule } from './orders/orders.module';
+import { CoreModule } from './core/core.module';
 
 @Module({
   imports: [
@@ -24,6 +25,7 @@ import { OrdersModule } from './orders/orders.module';
     ReceiptsModule,
     NotificationsModule,
     OrdersModule,
+    CoreModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/notifications/notifications.module.ts
+++ src/notifications/notifications.module.ts
@@ -1,9 +1,11 @@
-import { forwardRef, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { NotificationsService } from './notifications.service';
-import { OrdersModule } from 'src/orders/orders.module';
+//import { OrdersModule } from 'src/orders/orders.module';
+//import { forwardRef } from '@nestjs/common';
+import { CoreModule } from 'src/core/core.module';
 
 @Module({
-  imports: [forwardRef(() => OrdersModule)],
+  imports: [CoreModule],
   providers: [NotificationsService],
   exports: [NotificationsService],
 })
